ac9644b1ae0a82abb234dd4b75532551850ca73f
Lizabeth <23@18>
2026-06-16 15:10:28 +0300

    Задание 2. Переменные, математические функции, условные операторы
    - Считать потоком ввода три числа, введенных пользователем, и сохранить их в целочисленных переменных a, b и c
    - Вывести на консоль переменные, значения которых делятся на 5 (вида: a=25, c=15), если нет ни одного значений, делящегося на 5, вывести строку: "нет значений, кратных 5"
    - Вывести на консоль значение от целочисленного деления a на b (результат - целое число)
    - Вывести на консоль значение от деления a на b (результат - число с плавающей запятой)
    - Вывести на консоль значение от деления a на b, округленного до ближайшего целого в большую сторону
    - Вывести на консоль значение от деления a на b, округленного до ближайшего целого в меньшую сторону
    - Вывести на консоль значение от деления a на b, округленного до ближайшего целого математическим округлением
    - Вывести на консоль остаток от деления b на c
    - Вывести на консоль наименьшее значение из a и b
    - Вывести на консоль наибольшее значение из b и c

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Как тебя зовут?");
         Scanner in = new Scanner(System.in);
-        String name = in.next();
-        System.out.printf("Привет, %s!", name);
+        int a=0, b=0, c=0;
+        try {
+            System.out.print("Введите целое число a: ");
+            a = in.nextInt();
+            System.out.print("Введите целое число b: ");
+            b = in.nextInt();
+            System.out.print("Введите целое число c: ");
+            c = in.nextInt();
 
+            if (a % 5 == 0 || b % 5 == 0 || c % 5 == 0) {
+                if (a % 5 == 0)
+                    System.out.println("a=" + a);
+                if (b % 5 == 0)
+                    System.out.println("b=" + b);
+                if (c % 5 == 0)
+                    System.out.println("c=" + c);
+            } else
+                System.out.println("Нет значений, кратных 5");
+
+            System.out.println("(целочисленное) a / b = " + a/b);
+
+            System.out.println("a / b = " + (double) a / b);
+
+            System.out.println((int) Math.ceil((double) a / b));
+
+            System.out.println((int) Math.floor((double) a / b));
+
+            System.out.println(Math.round((double) a / b));
+
+            System.out.println("b % c = " + b % c);
+
+            System.out.println(Math.min(a, b));
+
+            System.out.println(Math.max(b, c));
+
+        }
+        catch(Exception e) {
+            System.err.print(e);
+        }
     }
 }
