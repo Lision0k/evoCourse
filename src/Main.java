10052224621925aa886adcbb24226e9bfe58f26e
Lizabeth <23@18>
2026-06-16 16:23:24 +0300

    Задание 4. Строки, циклы, условные операторы, (списки и мапы по желанию) Для решения задач нужно использовать методы класса String (не рассматривать строку как массив символов)

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,49 @@
-import java.util.Arrays;
-import java.util.Random;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[20];
-        Random rand = new Random();
-        for (int i = 0; i < 20; i++)
-            arr[i] = rand.nextInt(1,16);
+        Scanner in = new Scanner(System.in);
 
-        System.out.println(Arrays.toString(arr));
+        //Подсчитать сколько раз подстрока встречается в строке и вывести это значение на экран
+        System.out.print("Введите строку: ");
+        String str = in.nextLine();
+        System.out.print("Введите подстроку: ");
+        String sub_str = in.nextLine();
         int counter = 0;
-        for (int i = 1; i < 16; i++) {
-            for(int num : arr){
-                if(num == i)
-                    counter++;
+        while (true){
+            if(str.contains(sub_str)){
+                str = str.substring(str.indexOf(sub_str)+sub_str.length());
+                counter++;
             }
-            if(counter > 1)
-                System.out.printf("Число '%d' встречается %d раз(а)\n", i, counter);
-            counter = 0;
+            else break;
+        }
+        System.out.printf("Подстрока '%s' встречается %d раз(а)\n\n", sub_str, counter);
+
+        //Заменить в строке все слова "кака" и "бяка" на "вырезано цензурой"
+        System.out.println("Введите строку:");
+        str = in.nextLine();
+        str = str.replaceAll("кака", "вырезано цензурой");
+        str = str.replaceAll("бяка", "вырезано цензурой");
+        System.out.println(str + "\n");
+
+        //Ввести строку с датой формата: 31.12.2020
+        //Преобразовать строку даты в формат: 2020-12-31
+        System.out.println("Ведите дату в формате 'дд.мм.гггг'");
+        String date = in.nextLine();
+        String[] datenums = date.split("\\.");
+        System.out.printf("%s-%s-%s\n", datenums[2], datenums[1], datenums[0]);
+
+        //Сделать предыдущее задание с использованием классов Date и SimpleDateFormat и их соответствующих методов
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.M.yyyy");
+        try {
+            Date date1 = sdf.parse(date);
+            sdf = new SimpleDateFormat("yyyy-M-dd");
+            System.out.println(sdf.format(date1));
+        } catch (ParseException e) {
+            System.err.println(e);
         }
     }
 }
