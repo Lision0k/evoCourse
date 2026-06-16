47ab21d6dbb94ca755acea34c19924cdc10bcb23
Lizabeth <23@18>
2026-06-16 18:48:38 +0300

    Задание 3. Массивы, циклы, условные операторы, математические функции, (списки и мапы по желанию) выполненное с использованием мапа

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Main {
@@ -9,6 +11,25 @@ public class Main {
             arr[i] = rand.nextInt(1,16);
 
         System.out.println(Arrays.toString(arr));
+
+        // Решение через Map
+        Map<Integer, Integer> map = new HashMap<>();
+        int count;
+        for (int num : arr) {
+            if (map.containsKey(num)) {
+                count = map.get(num);
+                count++;
+                map.put(num, count);
+            }
+            else map.put(num, 1);
+        }
+        for (Integer key : map.keySet()){
+            if (map.get(key)>1)
+                System.out.printf("Число '%d' встречается %d раз(а)\n", key, map.get(key));
+            }
+
+        // Решение через вложенные циклы
+        /*
         int counter = 0;
         for (int i = 1; i < 16; i++) {
             for(int num : arr){
@@ -19,5 +40,6 @@ public class Main {
                 System.out.printf("Число '%d' встречается %d раз(а)\n", i, counter);
             counter = 0;
         }
+        */
     }
 }
