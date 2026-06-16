b20a267637e89d1f6d9ac74057671ae905b24d60
Lizabeth <23@18>
2026-06-16 15:31:36 +0300

    Задание 3. Массивы, циклы, условные операторы, математические функции
    - Заполнить массив из 20 элементов случайными целыми значениями в диапазоне от 1 до 15 включительно
    - Вывести на экран содержимое массива
    - Вывести на экран значения, которые встречаются в массиве больше одного раза, в формате, вида: "Число '3' встречается 2 раза"

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Как тебя зовут?");
-        Scanner in = new Scanner(System.in);
-        String name = in.next();
-        System.out.printf("Привет, %s!", name);
+        int[] arr = new int[20];
+        Random rand = new Random();
+        for (int i = 0; i < 20; i++)
+            arr[i] = rand.nextInt(1,16);
 
+        System.out.println(Arrays.toString(arr));
+        int counter = 0;
+        for (int i = 1; i < 16; i++) {
+            for(int num : arr){
+                if(num == i)
+                    counter++;
+            }
+            if(counter > 1)
+                System.out.printf("Число '%d' встречается %d раз(а)\n", i, counter);
+            counter = 0;
+        }
     }
 }
